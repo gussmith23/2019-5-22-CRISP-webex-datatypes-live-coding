4493657844a1bc51c71969aaffe1a0e44090ba7b
Gus Smith <gussmith@cs.washington.edu>
2019-05-17 10:07:48 -0700

    Schedule, lower, and build basic program

--- cast-and-add.py
+++ cast-and-add.py
@@ -6,3 +6,9 @@ import topi
 X = tvm.placeholder((3, ))
 Y = tvm.placeholder((3, ))
 Z = topi.add(X, Y)
+
+# Compile for LLVM (schedule, lower, and build)
+target = "llvm"
+schedule = tvm.create_schedule([Z.op])
+lowered_func = tvm.lower(schedule, [X, Y, Z])
+built_program = tvm.build(lowered_func, target=target)

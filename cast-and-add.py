02de220936deb5dbd6e98cbf198c09708f92aa5e
Gus Smith <gussmith@cs.washington.edu>
2019-05-17 18:37:48 -0700

    Create input numpy arrays

--- cast-and-add.py
+++ cast-and-add.py
@@ -1,6 +1,7 @@
 import tvm
 # TVM operator index, containing things like topi.add
 import topi
+import numpy as np
 
 # Our basic program: Z = X + Y
 X = tvm.placeholder((3, ))
@@ -12,3 +13,11 @@ target = "llvm"
 schedule = tvm.create_schedule([Z.op])
 lowered_func = tvm.lower(schedule, [X, Y, Z])
 built_program = tvm.build(lowered_func, target=target)
+
+# Create a device context
+context = tvm.context(target, 0)
+# Create random input arrays on the above context
+x = tvm.nd.array(np.random.rand(3).astype("float32"), ctx=context)
+y = tvm.nd.array(np.random.rand(3).astype("float32"), ctx=context)
+print("x:\t\t\t\t{}".format(x))
+print("y:\t\t\t\t{}".format(y))
